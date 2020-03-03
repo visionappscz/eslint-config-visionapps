d162081e47cac1e92cc57def8bb78af4bd70ff48
Bedrich Schindler <bedrich.schindler@gmail.com>
2020-03-03 09:32:56 +0100

    Update dependencies, enable react-hooks plugin and customize rules

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 module.exports = {
     parserOptions: {
-        ecmaVersion: 9,
+        ecmaVersion: 11,
         sourceType: 'module'
     },
     extends: [
         'airbnb',
-        'plugin:promise/recommended'
+        'plugin:promise/recommended',
+
+    ],
+    plugins: [
+        'react-hooks'
     ],
     rules: {
         'import/order': ['error', {
             'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
         }],
-        'no-multiple-empty-lines': ['error', { 'max': 1 }],
         'object-curly-newline': ['error', {
             'multiline': true,
             'minProperties': 2,
@@ -19,7 +22,13 @@ module.exports = {
         }],
         'object-curly-spacing': ['error', 'always'],
         'object-property-newline': ['error', { 'allowMultiplePropertiesPerLine': false }],
-        'sort-keys': ['error', 'asc'],
-        'no-undef-init': ['error']
+        'no-multiple-empty-lines': ['error', { 'max': 1 }],
+        'no-undef-init': ['error'],
+        'promise/catch-or-return': ['off'],
+        'promise/no-nesting': ['off'],
+        'react/jsx-props-no-spreading': ['off'],
+        'react-hooks/exhaustive-deps': ['error'],
+        'react-hooks/rules-of-hooks': ['error'],
+        'sort-keys': ['error', 'asc']
     }
 };
